7f17e35c103ba5e455ea97fd1d9238576fa47b2b
perrychen901105 <chenzhipeng901105@gmail.com>
2014-09-10 18:26:43 +0800

    add

--- Xork/Xork/XorkViewController.m
+++ Xork/Xork/XorkViewController.m
@@ -45,6 +45,31 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     
+    // 1
+    /*
+     *  Get the input string from the text view and convert it to a lower case string. This means that the commands "go north", "Go North" and "gO NoRtH" are all interpreted as the same command.
+     */
+    NSString *inputString = textField.text;
+    [inputString lowercaseString];
+    
+    // 2
+    /*
+     *  If the command is clear, erase the contents of the console. This is useful when the game's text has filled the screen and becomes uncomfortable to read.
+     */
+    if ([inputString isEqualToString:@"clear"]) {
+        [self.outputTextView clear];
+    }
+    
+    // 3
+    /*
+     *  For all other inputs, simply print the string to ConsloleTextView. Notice that you're using SetText:concatenate instead of setting the UITextView's text property directly.By default, the text view overwrites everything when you set its text property. Passing YES into setText:concatenate: preserves the text that was showing before.
+     */
+    else {
+        [self.outputTextView setText:inputString concatenate:YES];
+    }
+    
+    [self.inputTextField setText:@""];
+    
     return YES;
 }
 
